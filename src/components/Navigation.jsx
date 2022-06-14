eb241016ca98c1b92da1f43d01cf389f41c3adaf
Safi <Nurzai.safi@gmail.com>
2022-06-14 17:44:11 +0430

    FEAT: Tailwind css added to the project

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,17 +1,40 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import PEHOLogo from '../assets/PNG/PEHOLogo_Black.png'
+import React from "react";
+import { Link } from "react-router-dom";
+import PEHOLogo from "../assets/PNG/PEHOLogo_Black.png";
 const Navigation = () => {
   return (
-    <div className='container'>
-      <div className='navigation'>
-        <img src={PEHOLogo} alt="Logo"  height='40px'/>
-        <ul>
-          <li/>
-        </ul>
+    <div className="container flex px-40">
+      <div className="navigation px-20 py-10">
+        <img src={PEHOLogo} alt="Logo" className="h-20" />
       </div>
+        <div className="hidden md:flex items-center space-x-1">
+          <a
+            href=""
+            className="py-4 px-6 text-green-500 border-b-4 border-green-500 font-semibold "
+          >
+            Home
+          </a>
+          <a
+            href=""
+            className="py-4 px-6 text-gray-500 font-semibold hover:text-green-500 transition duration-300"
+          >
+            Services
+          </a>
+          <a
+            href=""
+            className="py-4 px-6 text-gray-500 font-semibold hover:text-green-500 transition duration-300"
+          >
+            About
+          </a>
+          <a
+            href=""
+            className="py-4 px-6 text-gray-500 font-semibold hover:text-green-500 transition duration-300"
+          >
+            Contact Us
+          </a>
+        </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navigation
+export default Navigation;
