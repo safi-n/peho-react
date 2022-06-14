2307a55acecc0d3d70321cdc054885b945c73395
Safi <Nurzai.safi@gmail.com>
2022-06-14 16:20:14 +0430

    PEHO app created

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navigation = () => {
+  return (
+    <div>
+      <ul>
+        <li><img src="/src/assets" alt="" /></li>
+      </ul>
+    </div>
+  )
+}
+
+export default Navigation
