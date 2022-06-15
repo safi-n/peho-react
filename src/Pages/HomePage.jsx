682d4e9767777eb9dbfeee324782602b75cf8eaa
Safi <Nurzai.safi@gmail.com>
2022-06-15 18:02:05 +0430

    FEAT: Hero banner done

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,24 +1,28 @@
 import {Fragment} from 'react'
 import MainBanner from '../assets/XDFiles/MainBanner.jpg'
+import VideoHero from '../assets/PNG/videoHero.png'
 
 const HomePage = () => {
 
   return (
       <Fragment>
-        <div className='heroBg min-h-screen w-screen flex justify-center items-center '>
-          <div className='py-auto flex flex-row space-x-96'>
-            <div className="textBox box-border h-60 w-96 p-4 border-4 my-auto">
+        {/* HERO BANNER */}
+        <div className='heroBg min-h-screen w-screen flex justify-center items-center'>
+          <div className='py-auto flex flex-row space-x-4  min-h-fit w-screen'>
+            <div className="textBox my-auto">
               <h2 className='text-white text-4xl mb-12'>WHAT IS PEHO?</h2>
               <p className='text-white font-light'>PEHO is a Non governmental Non- profitable and Non- for profit organization with an Non independent legal identity. PEHO Aims to serve people and contribute  into Afghanistan’s rehabilitation.</p>
             </div>
-            <div className="designBox box-border h-60 w-96 p-4 border-4 my-auto ">
-              <div className="mockupBanner">
-                <div className="videoBox">
+            <div className="designBox my-auto ">
+              <div className="mockupBanner p-28">
+                <div className="videoBox bg-cover ">
+                  <img src={VideoHero} className='mainVideo' alt="Video hero" />
                 </div>
               </div>
             </div>
           </div>
         </div>
+        
       </Fragment>
   )
 }
