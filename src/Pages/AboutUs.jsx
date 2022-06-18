69b843bccaf68f81a94165878003035caee1e9aa
Safi <Nurzai.safi@gmail.com>
2022-06-18 16:30:09 +0430

    FEAT: PEHO value styling done

--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -2,11 +2,10 @@ import React from "react";
 import watofPic1 from "../assets/PNG/watofPic1.png";
 import watofPic2 from "../assets/PNG/watofPic2.png";
 import watofPic3 from "../assets/PNG/watofPic3.png";
-import afghanIcon from '../assets/PNG/AfghanIcon.png'
-import accountabilityIcon from '../assets/PNG/AccountabiltyIcon-01.png'
-import trustIcon from '../assets/PNG/handshakeIcon-01.png'
-import equalIcon from '../assets/PNG/EqualOpportunity-01.png'
-
+import afghanIcon from "../assets/PNG/AfghanIcon.png";
+import accountabilityIcon from "../assets/PNG/AccountabiltyIcon-01.png";
+import trustIcon from "../assets/PNG/handshakeIcon-01.png";
+import equalIcon from "../assets/PNG/EqualOpportunity-01.png";
 
 const AboutUs = () => {
   return (
@@ -46,30 +45,58 @@ const AboutUs = () => {
       </div>
 
       <div className="peho-value-container flex flex-col items-center">
-        <div className="peho-value-title">
-          <h2 className="text-4xl">PEHO's <span className="mainColor">VALUE</span></h2>
+        {/* Title */}
+        <div className="peho-value-title mb-20">
+          <h2 className="text-4xl">
+            PEHO's <span className="mainColor">VALUE</span>
+          </h2>
         </div>
-        <div className="value-boxes flex flex-row">
-          <div className="value-box-col-1 border p-10">
+        {/* ------------------------ BOXES */}
+        <div className="value-boxes flex flex-row space-x-8">
+          {/* Accountability */}
+          <div className="value-box-col-1 p-3 flex flex-col">
+            <span className=" h-28"></span>
+            <div className="value-box-image-accountabilty bg-white drop-shadow-md p-3">
+              <img src={accountabilityIcon} alt="" className="mb-4"/>
+              <h2 className="text-lg mb-2 secondaryColor">Accountability</h2>
+              <p className="font-light text-sm">
+                PEHO Follow the fact and condition of being responsible of
+                accountability in all its deals and relationships.
+              </p>
+            </div>
+          </div>
+
+          {/* Trust & Equal */}
+          <div className="value-box-col-2  flex flex-col p-3">
+            <div className="value-box-image-trust bg-white drop-shadow-md p-3">
+              <img src={trustIcon} alt="" className="mb-4"/>
+              <h2 className="text-lg mb-2 secondaryColor">Trust</h2>
+              <p className="font-light text-sm">
+                PEHO’s impact and its well-known brand as a trusted organization
+                is our most treasured asset.
+              </p>
+            </div>
             <span className="h-20"></span>
-            <div className="value-box-image">
-
-            </div>
-            <span></span>
-          </div>
-          <div className="value-box-col-2">
-            <div className="value-box-image flex flex-col">
-              <img src= alt="" />
-            </div>
-            <span></span>
-            <div className="value-box-image">
-
+            <div className="value-box-image-equality p-3 bg-white drop-shadow-md">
+              <img src={equalIcon} alt="" className="mb-4" />
+              <h2 className="text-lg mb-2 secondaryColor">Equal opportunities</h2>
+              <p className="font-light text-sm">
+                PEHO has a nondiscriminatory policy for internal and external
+                relationships.
+              </p>
             </div>
           </div>
-          <div className="value-box-col-3">
-            <span></span>
-            <img src={accountabilityIcon} alt="" />
-            <span></span>
+          {/* Afghan Led */}
+          <div className="value-box-col-3  flex flex-col p-3">
+            <span className="h-28"></span>
+            <div className="value-box-image-afghanLed bg-white drop-shadow-md p-3">
+              <img src={afghanIcon} alt="afghan Icon" className="mb-4"/>
+              <h2 className="text-lg mb-2 secondaryColor">Afghan Led</h2>
+              <p className="font-light text-sm">
+                PEHO is an Afghan-led and Afghan-managed organization to help
+                Afghan women with health problems.
+              </p>
+            </div>
           </div>
         </div>
       </div>
