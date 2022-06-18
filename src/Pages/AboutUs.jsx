15b3b03b49f0c17153d3d40aaaec03e9bd298a02
Safi <Nurzai.safi@gmail.com>
2022-06-18 13:26:18 +0430

    FEAT: about us page begin Hero container

--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
 
 const AboutUs = () => {
   return (
-    <div>AboutUs</div>
-  )
-}
+    <div className="container border px-30 mx-auto w-screenss">
+      <div className="watof-container h-screen flex flex-row">
+        <div className="watof-content">
+          <h2>
+            WE ARE TEAM OF <span className="mainColor">PROFESSIONALS</span>
+          </h2>
+          <p>
+            all members of PEHO are expert and innovative Afghans they have
+            educated and entitled by diploma. PEHO member are people who have
+            always had enough credibility in their work environment and social
+            life. Having a good and moral relationship that benefit’s own self
+            and others is one of the most important principal’s of their social
+            life and they are committed to this principle.
+          </p>
+        </div>
+        <div className="watof-pics">
+          <div className="watof-mockup">
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default AboutUs
+export default AboutUs;
