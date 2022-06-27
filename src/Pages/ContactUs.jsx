093e0276be20d354d1d84597655e424a5407e5d4
Safi <Nurzai.safi@gmail.com>
2022-06-27 09:22:34 +0430

    FEAT: Contact us page started

--- src/Pages/ContactUs.jsx
+++ src/Pages/ContactUs.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 
 const ContactUs = () => {
   return (
-    <div className='container px-30 mx-auto w-screens mb-32'>
-      
+    <div className='w-screen border h-screen'>
+      <div className="contact-us-container">
+        <div className="form-contact-us">
+
+        </div>
+
+        <div className="bg-contact-us">
+          
+        </div>
+      </div>
     </div>
   )
 }
