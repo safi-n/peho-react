4381f6100d313087e9e79f1929618d380f8a6df6
Safi <Nurzai.safi@gmail.com>
2022-06-27 09:51:31 +0430

    FEAT: Form and image container added

--- src/Pages/ContactUs.jsx
+++ src/Pages/ContactUs.jsx
@@ -1,19 +1,28 @@
-import React from 'react'
+import React from "react";
+import Footer from "../components/Footer";
 
 const ContactUs = () => {
+  const formSubmitHandler = {};
   return (
-    <div className='w-screen border h-screen'>
-      <div className="contact-us-container">
-        <div className="form-contact-us">
+    <div className="container">
+      <div className="contact-us-container h-screen flex flex-col">
 
+        <div className="form-container-contact-us p-12">
+          <form onSubmit={formSubmitHandler}>
+            <div className="input-field-text">
+              <label htmlFor="Name">Name</label>
+              <input type="text" className="text-input" />
+            </div>
+          </form>
         </div>
 
         <div className="bg-contact-us">
-          
+          <h3>how are you doing?</h3>
         </div>
       </div>
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default ContactUs
+export default ContactUs;
