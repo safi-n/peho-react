ef3db03cbf7ea7aaeddb17eb422327cd8215c0c0
Safi <Nurzai.safi@gmail.com>
2022-06-27 14:26:16 +0430

    FEAT: Contact us page img and form

--- src/Pages/ContactUs.jsx
+++ src/Pages/ContactUs.jsx
@@ -1,7 +1,9 @@
-import React, {useState} from "react";
+import React, {useState, useRef} from "react";
 import Footer from "../components/Footer";
+import contactUsImg from '../assets/PNG/contactUsImg.png'
 
 const ContactUs = () => {
+  const isMounted = useRef(true)
   const [formData, setFormData] = useState({
     name: '',
     number: '',
@@ -9,9 +11,21 @@ const ContactUs = () => {
     country: {}
   })
 
-  const formSubmitHandler = {
+  const {
+    name,
+    email,
+    number,
+    country
+  } = formData
+
+  const onSubmit = (e) => {
+    e.preventDefault();
     
   };
+
+  const onMutated = (e) => {
+
+  }
   return (
     <div className="container">
       <div className="contact-us-container h-screen flex flex-row space-x-8 border border-green-500">
@@ -24,28 +38,28 @@ const ContactUs = () => {
           {/* form start */}
           <div className="form-contact-us mx-auto mb-8">
             <form
-              onSubmit={formSubmitHandler}
+              onSubmit={onSubmit}
               className="contactUsForm border mt-10 p-6 mx-auto"
             >
               <div className="input-field-text flex flex-col">
                 <label htmlFor="Name" className="mb-2 font-light">
                   Name
                 </label>
-                <input type="text" value={formData} id='phoneNumber' className="text-input p-2 border" />
+                <input type="text" value={formData} id='phoneNumber' className="text-input p-2 border" onChange={onMutated}/>
               </div>
 
               <div className="input-field-text flex flex-col mt-9">
                 <label htmlFor="Email" className="mb-2 font-light">
                   Email
                 </label>
-                <input type="email" id='phoneNumber' value={formData} className="text-input p-2 border" />
+                <input type="email" id='phoneNumber' value={formData} className="text-input p-2 border" onChange={onMutated}/>
               </div>
 
               <div className="input-field-text flex flex-col mt-9">
                 <label htmlFor="Email" className="mb-2 font-light">
                   Number
                 </label>
-                <input type="number" id='phoneNumber' value={formData} className="text-input p-2 border" />
+                <input type="number" id='phoneNumber' value={formData} className="text-input p-2 border" onChange={onMutated}/>
               </div>
 
               <div className="input-field-text flex flex-col mt-9">
@@ -68,7 +82,9 @@ const ContactUs = () => {
         </div>
 
         <div className="bg-contact-us">
-          <div className="img-contact-us"></div>
+          <div className="img-contact-us">
+            <img src={contactUsImg}  alt="contact us" />
+          </div>
         </div>
       </div>
       <Footer />
